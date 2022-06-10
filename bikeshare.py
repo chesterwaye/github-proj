7d33eb3454bb8feb661832f9e45dc138aa102b15
Chester <cwaye2000@gmail.com>
2022-06-10 16:17:02 -0400

    add comments to months

--- bikeshare.py
+++ bikeshare.py
@@ -7,7 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
+#Months of data collection
 months = ['january', 'february', 'march', 'april', 'may', 'june']
 
 days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday','all']
